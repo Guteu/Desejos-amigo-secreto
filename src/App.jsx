b7892348fd3305adb51f2960b1089b70be2a385b
Guteu <danielguerra1711@gmail.com>
2024-12-09 21:56:35 -0300

    v1.0.1
    atualizei alguns textos e centralizei o texto no botão de fechar presentInfoBox

--- src/App.jsx
+++ src/App.jsx
@@ -57,12 +57,12 @@ function App() {
 		<>
 			<Header />
 			<main>
-				<PresentBox name="Pix" imgURL={pix01} whereToFind="no seu app do banco" details="O amigo secreto é de 50 reais, mas lembre-se que o céu é o limite" images={presentPix} />
-				<PresentBox name="Chuteira de Futsal" imgURL={chuteira02} whereToFind="Tanto online quanto em loja fisica, como a umbro e a nike" details="Eu calço 39-40" images={presentChuteira} />
+				<PresentBox name="Pix" imgURL={pix01} whereToFind="no seu app do banco" details="O amigo secreto é de 50 reais, mas lembre-se que o céu é o limite. Minha chave pix é meu email: danielguerra1711@gmail.com" images={presentPix} />
+				<PresentBox name="Chuteira de Futsal" imgURL={chuteira02} whereToFind="Tanto online quanto em loja fisica, como a umbro e a nike" details="Eu calço 39-40. As chuteiras de futsal da umbro são as melhores" images={presentChuteira} />
 				<PresentBox name="Mouse Gamer" imgURL={mouseGamer02} whereToFind="loja online como kabum, amazon e mercado livre" details="eu não me importo com o led no mouse, mas esses botões laterais são bem legais" images={presentMouseGamer} />
-				<PresentBox name="Camisa de Programação" imgURL={camisaProgramador} whereToFind="loja online" details="se não conseguir achar o da imagem tudo bem, mas eu quero uma preta, porque a minha branca suja muito facil, se tiver while (alive) ou while (!dead) na camisa, não pode ter repeat" images={presentCamisaProgramador} />
+				<PresentBox name="Camisa de Programação" imgURL={camisaProgramador} whereToFind="loja online" details="O tamanho das minhas camisas é M. Se não conseguir achar o da imagem tudo bem, mas eu quero uma preta, porque a minha branca suja muito facil, se tiver while (alive) ou while (!dead) na camisa, não pode ter repeat" images={presentCamisaProgramador} />
 				<PresentBox name="Bola de Futsal" imgURL={bolaFutsal} whereToFind="loja online ou fisica, como adidas, nike, penalty ou decathlon" details="Prefiro bola de futsal mas eu aceito de futebol tambem, e eu não quero a bola mini" images={presentBolaFutsal} />
-				<PresentBox name="USB de qualidade" imgURL={usbHub} whereToFind="loja online tipo mercado livre e a amazon" details="USB hub barato é ruim e dura menos de um mês, então não compre os de 15 reais porque só vão durar 15 dias" images={presentUsbHub} />
+				<PresentBox name="USB hub de qualidade" imgURL={usbHub} whereToFind="loja online tipo mercado livre e a amazon" details="USB hub barato é ruim e dura menos de um mês, então não compre os de 15 reais porque só vão durar 15 dias" images={presentUsbHub} />
 				<PresentBox name="Microfone de mesa USB" imgURL={microfone} whereToFind="loja online tipo mercado livre e a amazon" details="O microfone tem que ser USB, porque eu uso notebook" images={presentMicrofone} />
 			</main>
 			
